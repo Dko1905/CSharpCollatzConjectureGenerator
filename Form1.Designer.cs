4345e2f64cb3b90377e602609a91449051d86ceb
Dko1905 <Daniel.florescu1905@gamil.com>
2019-09-17 07:34:22 +0200

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace numberGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.choseFileButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberBox = new System.Windows.Forms.NumericUpDown();
+            this.stRadio = new System.Windows.Forms.RadioButton();
+            this.mtRadio = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.numberBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            resources.ApplyResources(this.startButton, "startButton");
+            this.startButton.Name = "startButton";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // saveFileDialog1
+            // 
+            resources.ApplyResources(this.saveFileDialog1, "saveFileDialog1");
+            // 
+            // choseFileButton
+            // 
+            resources.ApplyResources(this.choseFileButton, "choseFileButton");
+            this.choseFileButton.Name = "choseFileButton";
+            this.choseFileButton.UseVisualStyleBackColor = true;
+            this.choseFileButton.Click += new System.EventHandler(this.ChoseFileButton_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // numberBox
+            // 
+            this.numberBox.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            resources.ApplyResources(this.numberBox, "numberBox");
+            this.numberBox.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numberBox.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numberBox.Name = "numberBox";
+            this.numberBox.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // stRadio
+            // 
+            resources.ApplyResources(this.stRadio, "stRadio");
+            this.stRadio.Checked = true;
+            this.stRadio.Name = "stRadio";
+            this.stRadio.TabStop = true;
+            this.stRadio.UseVisualStyleBackColor = true;
+            // 
+            // mtRadio
+            // 
+            resources.ApplyResources(this.mtRadio, "mtRadio");
+            this.mtRadio.Name = "mtRadio";
+            this.mtRadio.TabStop = true;
+            this.mtRadio.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.mtRadio);
+            this.Controls.Add(this.stRadio);
+            this.Controls.Add(this.numberBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.choseFileButton);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numberBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button choseFileButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numberBox;
+        private System.Windows.Forms.RadioButton stRadio;
+        private System.Windows.Forms.RadioButton mtRadio;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numberGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private UInt64 doCalculations(UInt64 number)
+        {
+            if (number % 2 == 0)
+            {
+                return number / 2;
+            }
+            else
+            {
+                return (number * 3) + 1;
+            }
+        }
+
+        public void setProgress(int progress)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<int>(setProgress), new object[] { progress });
+                return;
+            }
+            progressBar1.Value = progress;
+        }
+
+        private void singleThread()
+        {
+            Stream fs; // fileStream as a stream
+            StreamWriter sw;
+            try
+            {
+                fs = saveFileDialog1.OpenFile();
+                sw = new StreamWriter(fs, Encoding.ASCII);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"The save file is not correct! {ex.Message}", "Error", MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+                return;
+            }
+            UInt64 num;
+            for (UInt64 i = 0; i < numberBox.Value; i++)
+            {
+                num = i;
+                sw.Write(i);
+                while (num > 1)
+                {
+                    num = doCalculations(num);
+                    sw.Write(",");
+                    sw.Write(num);
+                }
+                sw.Write("\n");
+                if (i % 2 == 0)
+                {
+                    setProgress((int)((float)i / (float)numberBox.Value * 100));
+                }
+            }
+            setProgress(100);
+            sw.Close(); // also closeses underlying stream.
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            
+
+            progressBar1.Maximum = 100;
+            if (stRadio.Checked)
+            {
+                Task.Run( singleThread );
+            }            
+        }
+
+        private void ChoseFileButton_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+    }
+}
